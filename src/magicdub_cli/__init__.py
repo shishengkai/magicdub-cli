7d25b22eccaadea95c8b3ab8be66933e44d0993e
Aaron Ji <aaronjituo@gmail.com>
2026-09-23 07:31:40 +0800

    Bump version to 0.1.3

--- src/magicdub_cli/__init__.py
+++ src/magicdub_cli/__init__.py
@@ -1,3 +1,3 @@
 """magicdub-cli package version."""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
--- src/magicdub_cli/constants.py
+++ src/magicdub_cli/constants.py
@@ -7,7 +7,7 @@ from pathlib import Path
 SCHEMA_VERSION = 1
 ENGINE = "magicdub-cli"
 PROGRAM_NAME = "magicdub-cli"
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 CNY_QUANTUM = "0.00000001"
 
